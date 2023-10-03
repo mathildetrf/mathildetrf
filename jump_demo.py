1b8d7d88d2cd3c8e50d58176177225f0b016e872
mathilde <mathilde.truffer@unibe.ch>
2023-10-03 10:58:10 +0200

    size --> height

--- jump_demo.py
+++ jump_demo.py
@@ -1,14 +1,14 @@
 from turtle import forward, left, right, exitonclick
 
-def jump(size):
+def jump(height = 20):
     left(60)
-    forward(size)
+    forward(height)
     right(120)
-    forward(size)
+    forward(height)
     left(60)
 
 
 for size in range(20, 70, 5):
-    jump(size)
+    jump()
 
 exitonclick()
